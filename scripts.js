081d45b69266374a815f415c4769e15a0fd6c386
joef1sher <100483411+joef1sher@users.noreply.github.com>
2023-03-25 02:02:28 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,36 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const moreInfoButton = document.querySelector(".more-info");
-    const getStartedButton = document.querySelector(".get-started");
+document.addEventListener('DOMContentLoaded', function () {
+  var modal = document.getElementById('myModal');
+  var btn = document.getElementById('get-started');
+  var span = document.getElementsByClassName('close')[0];
 
-    moreInfoButton.addEventListener("click", function() {
-        alert("More Info clicked!");
+  if (btn) {
+    btn.addEventListener('click', function (event) {
+      event.stopPropagation();
+      modal.style.display = 'block';
     });
+  }
 
-    getStartedButton.addEventListener("click", function() {
-        alert("Get Started clicked!");
+  if (span) {
+    span.addEventListener('click', function (event) {
+      event.stopPropagation();
+      modal.style.display = 'none';
     });
+  }
+
+  window.onclick = function (event) {
+    if (event.target == modal) {
+      modal.style.display = 'none';
+    }
+  };
+
+  // More Info buttons
+  document.querySelectorAll('.more-info').forEach(function (button) {
+    button.addEventListener('click', function (event) {
+      event.stopPropagation();
+      const details = button.nextElementSibling;
+      if (details.classList.contains('info-content')) {
+        details.classList.toggle('open');
+      }
+    });
+  });
 });
