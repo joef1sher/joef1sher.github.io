2e667de5401dde074ec6c46bd6c800740209d856
joef1sher <100483411+joef1sher@users.noreply.github.com>
2023-03-25 12:26:14 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,19 +1,23 @@
-document.addEventListener('DOMContentLoaded', function () {
-  var modal = document.getElementById('myModal');
-  var btn = document.getElementById('get-started');
-  var span = document.getElementsByClassName('close')[0];
+const form = document.getElementById('contact-form');
 
-  btn.onclick = function () {
-    modal.style.display = 'block';
-  };
-
-  span.onclick = function () {
-    modal.style.display = 'none';
-  };
-
-  window.onclick = function (event) {
-    if (event.target == modal) {
-      modal.style.display = 'none';
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+  const formData = new FormData(form);
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', form.action);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      form.reset();
+      const successMessage = document.createElement('p');
+      successMessage.textContent = 'Thank you for contacting us. We will get back to you as soon as possible.';
+      form.parentNode.insertBefore(successMessage, form.nextSibling);
+    } else {
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = 'There was a problem submitting the form.';
+      form.parentNode.insertBefore(errorMessage, form.nextSibling);
     }
   };
+  xhr.send(formData);
 });
