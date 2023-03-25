c0190c62078d144c2add467624d15020b6ef05ff
joef1sher <100483411+joef1sher@users.noreply.github.com>
2023-03-25 02:11:55 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -3,32 +3,17 @@ document.addEventListener('DOMContentLoaded', function () {
   var btn = document.getElementById('get-started');
   var span = document.getElementsByClassName('close')[0];
 
-  if (btn) {
-    btn.onclick = function () {
-      modal.style.display = 'block';
-    };
-  }
+  btn.onclick = function () {
+    modal.style.display = 'block';
+  };
 
-  if (span) {
-    span.onclick = function () {
-      modal.style.display = 'none';
-    };
-  }
+  span.onclick = function () {
+    modal.style.display = 'none';
+  };
 
   window.onclick = function (event) {
     if (event.target == modal) {
       modal.style.display = 'none';
     }
   };
-
-  // More Info buttons
-  document.querySelectorAll('.more-info').forEach(function (button) {
-    button.addEventListener('click', function (event) {
-      event.stopPropagation(); // Add this line to stop event propagation
-      const details = button.nextElementSibling;
-      if (details.classList.contains('info-content')) {
-        details.classList.toggle('open');
-      }
-    });
-  });
 });
