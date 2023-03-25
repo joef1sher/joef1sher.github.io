82caa44cdfe0e0ca86e1c75c720363fbd09fa624
joef1sher <100483411+joef1sher@users.noreply.github.com>
2023-03-25 12:38:40 +0000

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,24 +1,23 @@
 const form = document.getElementById('contact-form');
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault(); // Prevent the form from submitting normally
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
   const formData = new FormData(form);
-  fetch('https://formspree.io/f/moqznrql', {
-    method: 'POST',
-    body: formData,
-    headers: {
-      'Accept': 'application/json'
-    }
-  })
-  .then(response => {
-    if (response.ok) {
-      alert('Your message has been sent!'); // Show a success message to the user
-      form.reset(); // Reset the form after submission
+  const xhr = new XMLHttpRequest();
+  xhr.open('POST', form.action);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.onreadystatechange = function() {
+    if (xhr.readyState !== XMLHttpRequest.DONE) return;
+    if (xhr.status === 200) {
+      form.reset();
+      const successMessage = document.createElement('p');
+      successMessage.textContent = 'Thank you for contacting us. We will get back to you as soon as possible.';
+      form.parentNode.insertBefore(successMessage, form.nextSibling);
     } else {
-      throw new Error('Network response was not ok.');
+      const errorMessage = document.createElement('p');
+      errorMessage.textContent = 'There was a problem submitting the form.';
+      form.parentNode.insertBefore(errorMessage, form.nextSibling);
     }
-  })
-  .catch(error => {
-    console.error('Error:', error);
-  });
+  };
+  xhr.send(formData);
 });
