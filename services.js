0bd428c7d0171199ac936e9fa01b8e1e242f21d0
joef1sher <100483411+joef1sher@users.noreply.github.com>
2023-03-25 02:21:38 +0000

    Update services.js

--- services.js
+++ services.js
@@ -1,8 +1,13 @@
+function toggleDetails(event) {
+  const button = event.target;
+  const details = button.nextElementSibling;
+  details.classList.toggle('open');
+}
+
 document.addEventListener('DOMContentLoaded', function () {
-  document.querySelectorAll('.more-info').forEach(function (button) {
-    button.addEventListener('click', function () {
-      const details = button.nextElementSibling;
-      details.classList.toggle('open');
-    });
+  const moreInfoButtons = document.querySelectorAll('.more-info');
+
+  moreInfoButtons.forEach((button) => {
+    button.addEventListener('click', toggleDetails);
   });
 });
